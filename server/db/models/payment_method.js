ef47688d79c1e3a224ebb7f5d7c9ec6af5b4898b
Jessica Kellogg <jmy.kellogg@gmail.com>
2016-09-13 10:28:31 -0500

    updated string

--- server/db/models/payment_method.js
+++ server/db/models/payment_method.js
@@ -5,6 +5,7 @@ var Sequelize = require('sequelize');
 var db = require('../_db');
 
 module.exports = db.define('payment_method', {
-    credit_card_tolken: Sequelize.INTEGER
+    token: Sequelize.STRING,
+    allowNull: false
 });
 
