c1878b81921b9e07cf9e1d6d21341e31c9e1d482
Your Name <you@example.com>
2016-09-20 21:12:55 +0000

    server temp needs

--- server/db/_db.js
+++ server/db/_db.js
@@ -2,9 +2,9 @@ var path = require('path');
 var Sequelize = require('sequelize');
 
 var env = require(path.join(__dirname, '../env'));
-var db = new Sequelize(env.DATABASE_URI, {
-  logging: env.LOGGING,
-  native: env.NATIVE
-});
+var db = new Sequelize('bookstack','bookstack', 'bookstack3', {dialect: 'postgres'})
+// logging: env.LOGGING,
+//  native: env.NATIVE
+//});
 
 module.exports = db;
